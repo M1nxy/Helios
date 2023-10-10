5e23e91722cb37f6ca1023108571047c4fb98332
M1nxy <m1nxd3v@gmail.com>
2023-10-10 19:12:56 +0100

    error handling interactionCreate.ts

--- src/events/interaction/interactionCreate.ts
+++ src/events/interaction/interactionCreate.ts
@@ -4,7 +4,11 @@ export default new Event('interactionCreate', false, (helios, interaction) => {
   if (interaction.isUserContextMenuCommand()) {
     const cmd = helios.userContext.get(interaction.commandName);
     if (cmd) {
-      cmd.execute(helios, interaction);
+      try {
+        cmd.execute(helios, interaction);
+      } catch (e){
+        console.log(e)
+      }
     } else {
       interaction.reply('Something went wrong!');
     }
